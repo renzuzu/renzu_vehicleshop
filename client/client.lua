0fa29aaf06feb4d038447e5fe95b87c15658fb9e
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-08-13 06:04:12 +0800

    Update client.lua

--- client/client.lua
+++ client/client.lua
@@ -67,11 +67,11 @@ end
 
 local neargarage = false
 function PopUI(name,v,event,text,server,shopname)
-    local text = text or ''
+    local text = text or 0
     local table = {
         ['event'] = event,
         ['title'] = name,
-        ['server_event'] = server,
+        ['server_event'] = server or false,
         ['unpack_arg'] = false,
         ['invehicle_title'] = 'Sell Vehicle '..text..'%',
         ['confirm'] = '[ENTER]',
@@ -97,7 +97,7 @@ CreateThread(function()
                 local dist = #(vec - GetEntityCoords(PlayerPedId()))
                 if dist < v.Dist and not inveh then
                     neargarage = true
-                    PopUI(v.title or v.name,vec,"vehicleshop")
+                    PopUI(v.title or v.name,vec,"vehicleshop",false)
                 end
             end
 
