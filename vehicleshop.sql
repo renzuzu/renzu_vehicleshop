e64fc8b134006b6d52ce2b167a0c94f5be03b305
renzuzu <82306584+renzuzu@users.noreply.github.com>
2021-11-23 16:12:35 +0800

    Update vehicleshop.sql

--- vehicleshop.sql
+++ vehicleshop.sql
@@ -9,7 +9,7 @@ CREATE TABLE `owned_vehicles` (
 	`plate` VARCHAR(12) NOT NULL COLLATE 'utf8mb4_bin',
 	`vehicle` LONGTEXT NULL DEFAULT NULL COLLATE 'utf8mb4_bin',
 	`impound` INT(1) NOT NULL DEFAULT '0',
-	`stored` INT(1) NOT NULL DEFAULT '0',
+	`stored` tinyint(1) NOT NULL DEFAULT '0',
 	`garage_type` VARCHAR(50) NULL DEFAULT 'car' COLLATE 'utf8mb4_bin',
 	`garage_id` VARCHAR(50) NULL DEFAULT 'A' COLLATE 'utf8mb4_bin',
 	PRIMARY KEY (`plate`) USING BTREE,
@@ -24,7 +24,7 @@ ADD `garage_id` varchar(32) NOT NULL DEFAULT 'A';
 
 
 ALTER TABLE owned_vehicles
-ADD job varchar(32) NOT NULL DEFAULT 'civ';
+ADD `job` varchar(32) NOT NULL DEFAULT 'civ';
 
 ALTER TABLE owned_vehicles
-ADD `stored` varchar(32) NOT NULL DEFAULT 1;
+ADD `stored` tinyint(1) NOT NULL DEFAULT 1;
